a1e8e595b6417c05900ad274f98c944812781e11
Fabio Giannelli <giannellifabio93@gmail.com>
2021-02-12 12:49:23 +0100

    Added Leonids and two particles system in main activity

---
+++ app/src/main/java/it/italiaonline/animationtestapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package it.italiaonline.animationtestapp
+
+import android.animation.ObjectAnimator
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.core.view.doOnPreDraw
+import androidx.databinding.DataBindingUtil
+import com.plattysoft.leonids.ParticleSystem
+import it.italiaonline.animationtestapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val inflater = LayoutInflater.from(this)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.root.doOnPreDraw {
+            ParticleSystem(this, 80, R.drawable.piano, 10000)
+                .setScaleRange(0.2f, 0.2f)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 180, 180)
+                .setRotationSpeed(144F)
+                .setAcceleration(0.00003f, 90)
+                .emit(findViewById(R.id.piano_emitter_right), 4);
+
+            ParticleSystem(this, 80, R.drawable.sound_wave, 10000)
+                .setScaleRange(0.2f, 0.2f)
+                .setSpeedModuleAndAngleRange(0f, 0.3f, 0, 0)
+                .setRotationSpeed(144F)
+                .setAcceleration(0.00003f, 90)
+                .emit(findViewById(R.id.piano_emitter_left), 4);
+        }
+    }
+}
